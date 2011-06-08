46d32bb8469604a5836ff70b6ba74c41b60ea032
unknown <ediaz@.(none)>
2011-06-07 23:18:19 -0400

    modifica loader para hacerla "mas funcional".

--- curso-de-programacion-cap-19/loader.py
+++ curso-de-programacion-cap-19/loader.py
@@ -1,9 +1,10 @@
 nemonicos = ['ALTO','LIM','ACUM','MUL','RES','DIV','IMPR','LEER','ALM','RR0','RR1','RSUM','RMUL','RRES','RDIV',
         'AR0','AR1','INC','DEC','INCR0','DECR0','INCR1','DECR1','SALTA','SSC','SSN','SSN','SRMA','SRME''SRIG']
 		
-memoria = []
+
 
 def loader(nombre_archivo):
+	memoria = []
 	archivo =open(nombre_archivo)
 	lineas = archivo.readlines()
 	for linea in lineas:
@@ -12,5 +13,6 @@ def loader(nombre_archivo):
 			memoria.append(nemonicos.index(codigo))
 		else:
 			memoria.append(int(codigo))
-			
+	return memoria
+	
 loader('tabla_cinco.a')
