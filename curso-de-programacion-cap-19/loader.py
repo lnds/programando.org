12659dae872c6eefa387467d2ab4160916678fe5
Eduardo Diaz <ediaz@dcc8090.cl>
2011-06-21 21:56:04 -0400

    corrige nemonico SSN (estaba 2 veces y faltaba SSP), y agrega REC

--- curso-de-programacion-cap-19/loader.py
+++ curso-de-programacion-cap-19/loader.py
@@ -1,5 +1,5 @@
-nemonicos = ['ALTO','LIM','ACUM','MUL','RES','DIV','IMPR','LEER','ALM','RR0','RR1','RSUM','RMUL','RRES','RDIV',
-        'AR0','AR1','INC','DEC','INCR0','DECR0','INCR1','DECR1','SALTA','SSC','SSN','SSN','SRMA','SRME''SRIG']
+nemonicos = ['ALTO','LIM','ACUM','MUL','RES','DIV','IMPR','LEER','ALM','REC','RR0','RR1','RSUM','RMUL','RRES','RDIV',
+        'AR0','AR1','INC','DEC','INCR0','DECR0','INCR1','DECR1','SALTA','SSC','SSN','SSP','SRMA','SRME''SRIG']
 		
 
 
